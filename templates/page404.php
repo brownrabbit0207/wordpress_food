90370af6ecd2364dfaa29b187870b7c0b5094b73
brownrabbit0207 <brownrabbit0207@gmail.com>
2018-12-22 00:00:00 +0900

    "2018-12-22 00:00"

---
+++ templates/page404.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: The template for displaying 404 pages (not found)
+*/
+global $page404_custom_template;
+$page404_custom_template = 'page404Template';
+$language = isset($_GET['lang']) ? $_GET['lang'] : '';
+
+add_action(
+    'theme_content_styles',
+    function () use ($page404_custom_template) {
+        theme_single_content_styles($page404_custom_template);
+    }
+);
+
+function theme_single_body_class_filter($classes) {
+    $classes[] = 'u-body u-xl-mode';
+    return $classes;
+}
+add_filter('body_class', 'theme_single_body_class_filter');
+
+function theme_single_body_style_attribute() {
+    return "";
+}
+add_filter('add_body_style_attribute', 'theme_single_body_style_attribute');
+if ($language) {
+    if (file_exists(get_stylesheet_directory() . '/' . 'template-parts/'. $page404_custom_template . '/translations/' . $language .'/single-content' . '.php')) {
+        $translations = '/translations/' . $language;
+    }
+}
+ob_start();
+get_template_part('template-parts/' . $page404_custom_template . $translations .'/single-content');
+$content = ob_get_clean();
+if (function_exists('renderTemplate')) {
+    renderTemplate($content, '', 'echo', 'custom');
+} else {
+    echo $content;
+}
+theme_layout_after('page404');
+
+ob_start();
+get_footer();
+$footer = ob_get_clean();
+if (function_exists('renderFooter')) {
+    renderFooter($footer, '', 'echo');
+} else {
+    echo $footer;
+}
+
+remove_filter('body_class', 'theme_single_body_class_filter');
