a9569ce4397380f34ef1a148feb549fb0c0dd66a
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-01-07 00:00:00 +0900

    "2019-01-07 00:00"

--- template-parts/page404Template/single-content-styles.php
+++ template-parts/page404Template/single-content-styles.php
@@ -1,4 +1,3 @@
-<!-- post styles -->
 
 <style> .u-section-1 {
   background-image: none;
@@ -13,6 +12,22 @@
 }
 .u-section-1 .u-text-2 {
   font-size: 1.875rem;
+  font-weight: 700;
+  line-height: 1;
+  text-transform: uppercase;
+  margin: 20px auto 0;
+}
+.u-section-1 .u-text-3 {
+  background-image: none;
+  font-size: 1.5rem;
+  width: 525px;
+  margin: 20px auto 0;
+}
+.u-section-1 .u-btn-1 {
+  border-style: solid;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 4px;
   background-image: none;
   align-self: center;
   margin: 30px 0 60px;
