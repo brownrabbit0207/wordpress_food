a438183a8cf33fec47b03aaa8f4a322712b4062b
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-04-27 00:00:00 +0900

    "2019-04-27 00:00"

--- template-parts/blogTemplate/blog-content-styles.php
+++ template-parts/blogTemplate/blog-content-styles.php
@@ -13,6 +13,22 @@
   min-height: 441px;
   grid-gap: 22px;
 }
+.u-section-1 .u-repeater-item-1 {
+  background-image: none;
+}
+.u-section-1 .u-container-layout-1 {
+  padding: 30px 20px;
+}
+.u-section-1 .u-text-1 {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.u-section-1 .u-image-1 {
+  height: 222px;
+  margin-top: 17px;
+  margin-bottom: 0;
+  visibility: hidden;
+}
 .u-section-1 .u-text-2 {
   margin-top: 20px;
   margin-bottom: 0;
