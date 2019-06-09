b94d051fb523f479fe303dfe0a84735029162e96
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-06-10 00:00:00 +0900

    "2019-06-10 00:00"

--- js/color-scheme-control.js
+++ js/color-scheme-control.js
@@ -1,4 +1,3 @@
-/**
  * Add a listener to the Color Scheme control to update other color controls to new values/defaults.
  * Also trigger an update of the Color Scheme CSS when a color is changed.
  */
