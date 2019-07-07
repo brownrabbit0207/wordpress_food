c10b4e02f31af85b9e55fea93a683572b752f4f6
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-07-08 00:00:00 +0900

    "2019-07-08 00:00"

--- template-parts/postTemplate/single-content.php
+++ template-parts/postTemplate/single-content.php
@@ -1,4 +1,3 @@
-<?php if (is_not_post_template($post)) { the_content(); } else { ?><?php global $wp_query, $post;
  ?>
 <?php $skip_min_height = false; ?><section class="u-align-center u-clearfix u-section-1" id="sec-a69d">
   <div class="u-clearfix u-sheet u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1"><!--post_details--><!--post_details_options_json--><!--{"source":""}--><!--/post_details_options_json--><!--blog_post-->
