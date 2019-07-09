76a7081d04c5d93d4f8d9f7c211e3e02de5d0a23
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-07-10 00:00:00 +0900

    "2019-07-10 00:00"

--- library/custom-widgets.php
+++ library/custom-widgets.php
@@ -3,6 +3,12 @@
 class WP_Categories_Widget extends WP_Widget {
 
     function __construct() {
+        parent::__construct(
+            'WP_Categories_Widget',
+            __('Categories', 'default'),
+            array( 'description' => __( 'A list of categories.', 'default' ), )
+        );
+    }
 
     public function widget( $args, $instance ) {
         render_custom_widget($args, $instance , 'WP_Widget_Categories');
