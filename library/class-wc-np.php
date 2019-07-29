ae99cef224a0a308e8d257d3c9c1afdf37d0279e
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-07-30 00:00:00 +0900

    "2019-07-30 00:00"

--- library/class-wc-np.php
+++ library/class-wc-np.php
@@ -1,13 +1,8 @@
+<?php
 /**
  * WC_NP class.
  */
 class WC_NP {
-
-	/**
-	 * Theme init.
-	 */
-	public static function init() {
-		// Remove default wrappers.
 		remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper' );
 		remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end' );
 
