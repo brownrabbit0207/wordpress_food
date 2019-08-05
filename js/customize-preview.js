8c4defb61eaeee20ada93f16c029a99b14bffd28
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-08-06 00:00:00 +0900

    "2019-08-06 00:00"

--- js/customize-preview.js
+++ js/customize-preview.js
@@ -1,13 +1,8 @@
+/**
  * Live-update changed settings in real time in the Customizer preview.
  */
 
 (function ($) {
-    var api = wp.customize;
-
-    // Site title.
-    api('blogname', function (value) {
-        value.bind(function (to) {
-            $('.site-title a').text(to);
         });
     });
 
