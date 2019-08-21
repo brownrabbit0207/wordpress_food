e6f5fc75e4db2078cf097a5d5ef03c9f4f06848a
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-08-22 00:00:00 +0900

    "2019-08-22 00:00"

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,6 +8,17 @@
  */
 ?>
 
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+			<span class="sticky-post"><?php _e( 'Featured', 'website4829605' ); ?></span>
+		<?php endif; ?>
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_excerpt(); ?>
+
 	<?php twentysixteen_post_thumbnail(); ?>
 
 	<div class="entry-content">
