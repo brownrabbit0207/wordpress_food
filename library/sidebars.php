264ceef092ccfc0d9bfe7ca0c3655c8aac3083bb
brownrabbit0207 <brownrabbit0207@gmail.com>
2019-09-12 00:00:00 +0900

    "2019-09-12 00:00"

--- library/sidebars.php
+++ library/sidebars.php
@@ -8,6 +8,17 @@ function theme_widgets_init() {
     
 }
 add_action('widgets_init', 'theme_widgets_init');
+
+
+
+function theme_register_unregister_widget_filters($sidebar_id, $add = true) {
+    $widget_filters = array(
+        
+    );
+    if (isset($widget_filters[$sidebar_id])) {
+        if ($add) {
+            add_filter('widget_text', $widget_filters[$sidebar_id], 1000);
+        } else {
             remove_filter('widget_text', $widget_filters[$sidebar_id], 1000);
         }
     }
