c9f6681322f6f605bd27e9c0a423fcb6e18d2bfa
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-01-02 00:00:00 +0900

    "2020-01-02 00:00"

--- inc/customizer.php
+++ inc/customizer.php
@@ -13,22 +13,6 @@ function theme_customize_register($wp_customize) {
     global $theme_default_options;
     $wp_customize->get_setting('blogname')->transport = 'postMessage';
     $wp_customize->get_setting('blogdescription')->transport = 'postMessage';
-
-    if (isset($wp_customize->selective_refresh)) {
-        $wp_customize->selective_refresh->add_partial('blogname', array(
-            'selector' => '.site-title a',
-            'container_inclusive' => false,
-            'render_callback' => 'theme_customize_partial_blogname',
-        ));
-        $wp_customize->selective_refresh->add_partial('blogdescription', array(
-            'selector' => '.site-description',
-            'container_inclusive' => false,
-            'render_callback' => 'theme_customize_partial_blogdescription',
-        ));
-    }
-
-    // Add color scheme setting and control.
-    $wp_customize->add_setting('color_scheme', array(
         'default' => 'default',
         'sanitize_callback' => 'sanitize_text_field',
         'transport' => 'postMessage',
