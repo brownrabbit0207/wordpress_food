166da72a511fe9c4b63bd94eaa854308a92b5d06
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-02-05 00:00:00 +0900

    "2020-02-05 00:00"

--- template-parts/header-content.php
+++ template-parts/header-content.php
@@ -3,16 +3,11 @@
     <?php $logo = theme_get_logo(array(
             'default_src' => "/images/default-logo.png",
             'default_url' => "https://nicepage.com"
-      <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
-        <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
-          <svg class="u-svg-link" viewBox="0 0 24 24"><use xlink:href="#menu-hamburger"></use></svg>
-          <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
-</g></svg>
-        </a>
-      </div>
-      <div class="u-nav-container">
-        {menu}
-      </div>
+        )); $url = stripos($logo['url'], 'http') === 0 ? esc_url($logo['url']) : $logo['url']; ?><a <?php if (is_customize_preview()) echo 'data-default-src="' . esc_url($logo['default_src']) . '" '; ?>href="<?php echo $url; ?>" class="u-image u-logo u-image-1 custom-logo-link">
+      <img <?php if ($logo['svg']) { echo 'style="width:'.$logo['width'].'px"'; } ?>src="<?php echo esc_url($logo['src']); ?>" class="u-logo-image u-logo-image-1">
+    </a>
+    <?php
+            ob_start();
       <div class="u-nav-container-collapse">
         <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
           <div class="u-inner-container-layout u-sidenav-overflow">
