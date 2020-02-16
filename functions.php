eee7e753960be4a8669b45fe1030c758955ec368
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-02-17 00:00:00 +0900

    "2020-02-17 00:00"

--- functions.php
+++ functions.php
@@ -1,4 +1,3 @@
-<?php
 /**
  * Theme only works in WordPress 4.4 or later.
  */
@@ -18,6 +17,27 @@ locate_template(array('library/translations/class-theme-multi-languages.php'), t
 
 if (!function_exists('theme_setup')) {
     /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     *
+     * Create your own theme_setup() function to override in a child theme.
+     */
+    function theme_setup() {
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
+
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
+
+        /*
          * Enable support for custom logo.
          */
         add_theme_support('custom-logo', array(
--- templates/page404.php
+++ templates/page404.php
@@ -1,4 +1,3 @@
-<?php
 /*
 Template Name: The template for displaying 404 pages (not found)
 */
