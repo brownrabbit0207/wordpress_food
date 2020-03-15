155995e784eee08aacf1148d92652085f37c05f2
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-03-16 00:00:00 +0900

    "2020-03-16 00:00"

--- library/options.php
+++ library/options.php
@@ -1,4 +1,3 @@
-<?php
 defined('ABSPATH') or die;
 
 global $theme_options, $theme_templates_options, $theme_template_query, $theme_template_type_priority, $theme_default_options;
@@ -18,6 +17,27 @@ if(!function_exists('theme_woocommerce_enabled')) {
 function theme_add_template_option($type, $name, $caption, $type_priority = 10) {
     global $theme_templates_options, $theme_template_type_priority;
     $theme_template_type_priority[$type] = $type_priority;
+    $theme_templates_options[$type][$name] = esc_attr(urldecode($caption));
+}
+
+function theme_add_template_query_option($type, $name, $caption) {
+    global $theme_template_query;
+    $theme_template_query[$name] = esc_attr(urldecode($caption));
+}
+
+theme_include_lib('templates_options.php');
+
+$theme_options = array(
+    array(
+        'name' => __('Templates', 'default'),
+        'type' => 'heading'
+    )
+);
+
+function theme_compare_template_names($a, $b) {
+    global $theme_template_type_priority;
+    if ($theme_template_type_priority[$a] === $theme_template_type_priority[$b])
+        return strnatcasecmp($a, $b);
     return $theme_template_type_priority[$b] - $theme_template_type_priority[$a];
 }
 uksort($theme_templates_options, 'theme_compare_template_names');
--- template-parts/blogTemplate/blog-content-styles.php
+++ template-parts/blogTemplate/blog-content-styles.php
@@ -1,4 +1,3 @@
-<!-- index styles -->
 
 <style>.u-section-1 .u-sheet-1 {
   min-height: 561px;
@@ -18,6 +17,27 @@
 }
 .u-section-1 .u-container-layout-1 {
   padding: 30px 20px;
+}
+.u-section-1 .u-text-1 {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.u-section-1 .u-image-1 {
+  height: 222px;
+  margin-top: 17px;
+  margin-bottom: 0;
+  visibility: hidden;
+}
+.u-section-1 .u-text-2 {
+  margin-top: 20px;
+  margin-bottom: 0;
+}
+.u-section-1 .u-btn-1 {
+  background-image: none;
+  border-style: solid;
+  margin: 17px auto 0 0;
+  padding: 0;
+}
 .u-section-1 .u-container-layout-2 {
   padding: 30px 20px;
 }
